1e089be88c16922efee493f8cdf6a8b15260967a
Saman <pradhansaman2017@gmail.com>
2023-05-12 07:27:15 +0530

    new 5

--- U5/sprint-2/day-3/we/U5 S2 D4/models/blacklist.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const BlackListSchema = mongoose.Schema({
-  token: { type: String, require: true },
-});
-
-const BlackListModel = mongoose.model("blacklisting", BlackListSchema);
-
-module.exports = { BlackListModel };
